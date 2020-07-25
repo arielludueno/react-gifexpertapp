e38fdefde97e097f0337d12d22651f60c581baee
Dante Ludueno <ludueno@hotmail.com>
2020-07-25 15:41:48 -0300

    docs agregado

---
+++ docs/precache-manifest.c5cd901287ad6ef155534476b511b328.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b22cf2dc0dc796f2dff1e183b117e76e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f6b62c9fca3fa6e2904",
+    "url": "/static/css/main.73822793.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f6b62c9fca3fa6e2904",
+    "url": "/static/js/main.13c035b9.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
